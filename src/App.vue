bb96b32acbf15f5b8990f218e3511efaf124e926
NeginSal <neginsaljooghi@gmail.com>
2021-06-23 17:29:05 +0430

    adding toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <v-app>
-    <v-content>
+  <v-app >
+    <Navbar/>
+    <v-main class="orange lighten-5">
       <router-view></router-view>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
-
+  components :{
+    Navbar
+  },
   data: () => ({
     //
   }),
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+      <v-toolbar flat app color="orange lighten-4">
+          <v-toolbar-title class="text-uppercase deep-orange--text text--darken-4">
+              Vuetify Site
+      <v-icon color="deep-orange accent-4"> mdi-heart </v-icon>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn flat color="orange darken-1">
+              <span>Sign Out</span>
+              <v-icon right>mdi-logout</v-icon>
+          </v-btn>
+
+      </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
