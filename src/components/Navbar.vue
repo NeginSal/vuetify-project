414a4f9efb7546d13526f4603c1753a784bd5642
NeginSal <neginsaljooghi@gmail.com>
2021-06-26 16:59:29 +0430

    add navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,26 +1,34 @@
 <template>
   <nav>
-      <v-toolbar flat app color="orange lighten-4">
-          <v-toolbar-title class="text-uppercase deep-orange--text text--darken-4">
-              Vuetify Site
-      <v-icon color="deep-orange accent-4"> mdi-heart </v-icon>
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn flat color="orange darken-1">
-              <span>Sign Out</span>
-              <v-icon right>mdi-logout</v-icon>
-          </v-btn>
-
-      </v-toolbar>
+<!--Navigation-drawer-->
+    <v-navigation-drawer app v-model="drawer" class="deep-orange lighten-4" ></v-navigation-drawer>
+<!--Navigation-drawer-->
+<!--Toolbar-->
+    <v-app-bar app color="orange lighten-4">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="deep-orange darken-4"></v-app-bar-nav-icon>
+        <v-toolbar flat app color="orange lighten-4">
+            <v-toolbar-title class="text-uppercase deep-orange--text text--darken-4">
+                Vuetify Site
+                <v-icon color="deep-orange accent-4"> mdi-heart </v-icon>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="orange darken-1">
+                <span>Sign Out</span>
+                <v-icon right>mdi-logout</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </v-app-bar>
   </nav>
 </template>
 
 <script>
 export default {
+  name: 'App',
+  data: () => ({
+    drawer: false,
 
+  })
 }
 </script>
-
 <style>
-
 </style>
