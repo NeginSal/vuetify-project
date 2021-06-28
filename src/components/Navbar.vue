bbf248e64fbcd31aed7c51431ce1727b1705c816
NeginSal <neginsaljooghi@gmail.com>
2021-06-28 12:10:14 +0430

    making a list on navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,18 @@
 <template>
   <nav>
 <!--Navigation-drawer-->
-    <v-navigation-drawer app v-model="drawer" class="deep-orange lighten-4" ></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer" class="deep-orange lighten-4" >
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon class="red--text text--darken-4" large>{{link.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-tile-title class="red--text text--darken-4">{{link.text}}</v-list-tile-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 <!--Navigation-drawer-->
 <!--Toolbar-->
     <v-app-bar app color="orange lighten-4">
@@ -26,6 +37,12 @@ export default {
   name: 'App',
   data: () => ({
     drawer: false,
+    links: [
+      {icon: 'mdi-view-dashboard' ,text:'Dashboard', route: '/'},
+      {icon: 'mdi-folder-open' ,text:'My Projects', route: '/projects'},
+      {icon: 'mdi-human-greeting' ,text:'Team', route: '/team'},
+
+    ]
 
   })
 }
