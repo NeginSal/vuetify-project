d4b2b667eb14214970b36a5b9713cb15225fe39e
NeginSal <neginsaljooghi@gmail.com>
2021-06-30 19:06:11 +0430

    working with grid system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,28 @@
 <template>
-<div class="home">
-  <h1>Dashboard</h1>
+<div class="dashboard">
+  <h1 class="subheading purple--text  lighten-1--text">Dashboard</h1>
+  <v-container class="my-5">
+    <v-card flat class="pa-3 purple lighten-5">
+      <v-layout row wrap class="pa-3">
+        <v-flex xs12 md6>
+          <div class="caption purple--text">Project Title</div>
+          <div>create a new website</div>
+        </v-flex>
+        <v-flex xs6 sm4 md2>
+          <div class="caption purple--text">Person</div>
+          <div>Negin</div>
+        </v-flex>
+        <v-flex xs6 sm4 md2>
+          <div class="caption purple--text">Duo by</div>
+          <div>30 june 2021</div>
+        </v-flex>
+        <v-flex xs2 sm4 md2>
+          <div class="caption purple--text">Status</div>
+          <div>ongoing</div>
+        </v-flex>
+      </v-layout>
+    </v-card>
+  </v-container>
 </div>
 
 
