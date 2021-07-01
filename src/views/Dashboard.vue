a5a0ee9382804f6e0b501e8605c517d7bf02f509
NeginSal <neginsaljooghi@gmail.com>
2021-07-01 17:02:50 +0430

    Dummy Project Data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,24 +1,24 @@
 <template>
-<div class="dashboard">
+<div class="dashboard my-3 mx-3">
   <h1 class="subheading purple--text  lighten-1--text">Dashboard</h1>
   <v-container class="my-5">
-    <v-card flat class="pa-3 purple lighten-5">
-      <v-layout row wrap class="pa-3">
+    <v-card flat class="pa-3 ma-2 purple lighten-5" v-for="project in projects" :key="project.title">
+      <v-layout row wrap class="pa-3" :class="`pa-3 project ${project.status}`">
         <v-flex xs12 md6>
           <div class="caption purple--text">Project Title</div>
-          <div>create a new website</div>
+          <div>{{project.title}}</div>
         </v-flex>
         <v-flex xs6 sm4 md2>
           <div class="caption purple--text">Person</div>
-          <div>Negin</div>
+          <div>{{project.person}}</div>
         </v-flex>
         <v-flex xs6 sm4 md2>
           <div class="caption purple--text">Duo by</div>
-          <div>30 june 2021</div>
+          <div>{{project.due}}</div>
         </v-flex>
         <v-flex xs2 sm4 md2>
           <div class="caption purple--text">Status</div>
-          <div>ongoing</div>
+          <div>{{project.status}}</div>
         </v-flex>
       </v-layout>
     </v-card>
@@ -32,6 +32,27 @@
 
 
   export default {
+    data(){
+      return{
+        projects:[
+          {title:'create a new website', person:'Negin', due:'30 june 2021', status:'ongoing'},
+          {title:'Code up the homepage', person:'Chun Li', due:'10 july 2021', status:'complete'},
+          {title:'Design Video tumbnails', person:'Ryu', due:'20 Sep 2021', status:'complete'},
+          {title:'Create a Cummunity forum', person:'Guken', due:'10 Oct 2021', status:'overdue'},
+        ]
+      }
+    }
 
   }
 </script>
+<style scoped>
+   .project.complete{
+     border-left: 4px solid #00cc00;
+   }
+   .project.ongoing{
+     border-left: 4px solid #0099ff;
+   }
+   .project.overdue{
+     border-left: 4px solid #ff0000;
+   }
+</style>
