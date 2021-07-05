ab8b6cee60213238be2bc49979cac5631456b412
NeginSal <neginsaljooghi@gmail.com>
2021-07-05 16:16:13 +0430

    adding data for cards

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is Team page</h1>
+  <div class="team">
+    <h1 class="subheading text--grey">Team</h1>
+    <v-container class="my-5">
+
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      team:[
+        {name:'Negin',role:'Web developer'},
+        {name:'Ryu',role:'Graphic designer'},
+        {name:'Chun Li',role:'Web developer'},
+        {name:'Gouken',role:'Social media maverick'},
+        {name:'Yoshi',role:'Sales guru'},
+      ]
+    }
+  }
 
 }
 </script>
