8c2dc248987c3e50497a491683265ce826b6daf4
NeginSal <neginsaljooghi@gmail.com>
2021-07-08 17:15:02 +0430

    add data

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,14 +3,14 @@
     <h1 class="blue--text text--darken-4">This is Projects page</h1>
     <v-container class="my-5">
       <v-row justify="center">
-        <v-expansion-panels accordion>
-          <v-expansion-panel>
-            <v-expansion-panel-header>Project title</v-expansion-panel-header>
+        <v-expansion-panels >
+          <v-expansion-panel  v-for="project in projects" :key="project.title">
+            <v-expansion-panel-header class="cyan--text text--darken-4">{{project.title}}</v-expansion-panel-header>
             <v-expansion-panel-content>
-              <v-card flat>
-                <v-card-text class="px-4 blue--text">
-                  <div class="font-weight-bold">duo by xx.xx.xx</div>
-                  <div>info</div>
+              <v-card flat color="blue lighten-4">
+                <v-card-text class="px-4">
+                  <div class="font-weight-bold blue--text">duo by {{project.duo}}</div>
+                  <div class="teal--text text--darken-4">{{project.content}}</div>
                 </v-card-text>
               </v-card>
             </v-expansion-panel-content>
@@ -19,11 +19,23 @@
       </v-row>
     </v-container>
   </div>
-</template><script>
+</template>
+
+<script>
   export default {
     data(){
       return{
-
+        projects:[
+          {title:'create a new website', person:'Negin', due:'30 june 2021', status:'ongoing',content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+          {title:'Code up the homepage', person:'Chun Li', due:'10 july 2021', status:'complete',content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+          {title:'Design Video tumbnails', person:'Ryu', due:'20 Sep 2021', status:'complete',content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+          {title:'Create a Cummunity forum', person:'Guken', due:'10 Oct 2021', status:'overdue',content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+        ]
+      }
+    },
+    methods: {
+      sortBy(prop){
+        this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
       }
     }
   }
