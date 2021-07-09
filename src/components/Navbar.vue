e4ca1a87ca640eee8fafa0272562014fd0d1ed09
NeginSal <neginsaljooghi@gmail.com>
2021-07-09 19:03:11 +0430

    add menus

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -31,6 +31,23 @@
                 <v-icon color="deep-orange accent-4"> mdi-heart </v-icon>
             </v-toolbar-title>
             <v-spacer></v-spacer>
+
+            <!--drop down-->
+            <v-menu offset-y >
+              <template v-slot:activator="{ on, attrs }" >
+                <v-btn flat color="orange darken-1" v-bind="attrs" v-on="on" class="mx-2">
+                  <v-icon left>mdi-arrow-down-drop-circle-outline</v-icon>
+                  <span>Menu</span>
+                </v-btn>
+              </template>
+              <v-list color="orange lighten-4" class="deep-orange--text text--darken-4">
+                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route" >
+                     <v-list-item-title>{{ link.text }}</v-list-item-title>
+                 </v-list-item>
+              </v-list>
+            </v-menu>
+
+
             <v-btn flat color="orange darken-1">
                 <span>Sign Out</span>
                 <v-icon right>mdi-logout</v-icon>
