a5ca9b42d773dbd622cd8afabeb5edfde5b9e7ab
NeginSal <neginsaljooghi@gmail.com>
2021-07-10 15:22:23 +0430

    complete Popup component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,6 +9,9 @@
           </v-avatar>
           <p class="black--text text-center subheading mt-1">Negin</p>
         </v-flex>
+        <v-flex>
+          <popup />
+        </v-flex>
       </v-layout>
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
@@ -58,8 +61,10 @@
 </template>
 
 <script>
+import Popup from './Popup.vue'
+
 export default {
-  name: 'App',
+  components: { Popup },
   data: () => ({
     drawer: false,
     links: [
--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+    <div class="text-center">
+        <v-dialog v-model="dialog" width="500">
+           <template v-slot:activator="{ on, attrs }">
+              <v-btn color="red" dark v-bind="attrs" v-on="on">
+                 Add new project
+              </v-btn>
+          </template>
+          <v-card >
+              <v-card-title class="text-h5 red ">
+                  <v-icon left>mdi-plus-box-outline</v-icon>
+                    new project
+              </v-card-title>
+              <v-card-text class="black--text mt-4">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </v-card-text>
+           </v-card>
+        </v-dialog>
+    </div>
 </template>
 
 <script>
@@ -11,3 +28,14 @@ export default {
 <style>
 
 </style>
+      <v-dialog max-width="600px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn flat slot="activator" class="success"></v-btn>
+              <v-card>
+              <v-card-title>
+                  <h2>Add a new project</h2>
+
+              </v-card-title>
+             </v-card>
+           </template>
+      </v-dialog>
